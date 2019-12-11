69399fccf25aaa2410a53d9886cd8005e61a122c
Will Baker <willbaker698@gmail.com>
2019-12-11 15:49:47 +0000

    Test Socket.io

--- bin/io.js
+++ bin/io.js
@@ -1,5 +1,14 @@
-let io = require('socket.io');
+let socket_io = require('socket.io');
+let io = socket_io();
 
 io.on('connection', () => {
    console.log('Connected');
 });
+
+let ioc = require('socket.io-client')('http://localhost:3000');
+
+ioc.on('connect', () => {
+   console.log('client-connected');
+});
+
+module.exports.io = io;
