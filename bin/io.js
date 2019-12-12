41ad6e5d954e75ee92e14f777dd87eb2a80c32c8
Will <willbaker698@gmail.com>
2019-12-12 16:38:33 +0000

    Update IO

--- bin/io.js
+++ bin/io.js
@@ -1,11 +1,17 @@
 let socket_io = require('socket.io');
 let io = socket_io();
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
    console.log('Connected');
+
+   socket.on('test-data', (data) => {
+      let dataStr = JSON.stringify(data);
+
+      console.log(dataStr);
+   });
 });
 
-let ioc = require('socket.io-client')('http://localhost:3000');
+let ioc = require('socket.io-client')('http://localhost');
 
 ioc.on('connect', () => {
    console.log('client-connected');
